e02e4f2e7d98576e1c82372a948d6fcf681c1cd3
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-05-23 09:24:35 -0700

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+User Stories
+
+A User should see two buttons side-by-side on the home page with an option to select either existing user or new user.
+
+A User will be sent to either the login page, if they're an existing user, or a signup page if they're a new user to my app.
+
+A User will be sent to a page with a search bar and the ability to search albums.
+
+The results of the users' search will be yielded on the page similar to our CatchPhrase.ly app.
+
+The videos that the user will see will be interviews by the artist regarding the searched album.
--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 //Back-End Server, requires, routes
+var express = require("express"),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	_ = require("underscore"),
+	path = require("path"),
+	bcrypt = require("bcrypt");
 
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname, '/public'));
+
+var views = path.join(__dirname, "views");	
+
+app.get("/", function (req, res) {
+	var homePath = path.join(views, "home.html");
+	res.sendFile(homePath);
+});
+
+app.get("/signup", function (req, res) {
+	var signupPath = path.join(views, "signup.html");
+	res.sendFile(signupPath);
+});
+
+app.get("/login", function (req, res) {
+	var loginPath = path.join(views, "login.html");
+	res.sendFile(loginPath);
+});
+
+app.get("/activities", function (req, res) {
+	var activitiesPath = path.join(views, "activities.html");
+	res.sendFile(activitiesPath);
+})
+
+app.listen(3000, function () {
+	console.log("running");
+})
